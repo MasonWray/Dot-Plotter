5e8758a55a47366714b04a8ddc7003c622cee998
Mason Wray <masonwray.mw@gmail.com>
2021-02-16 11:10:32 -0600

    Fixed File Check
    Loading pixel data no longer checks property of undef file object when file selector is closed.

--- web-app/src/components/FileSelector.js
+++ web-app/src/components/FileSelector.js
@@ -1,10 +1,9 @@
 function FileSelector(props) {
     const handleFileChange = (e) => {
         var file = e.target.files[0];
-        // props.setFile(file);
 
-        // Load pixel date into array when file is selected
-        if (file.type) {
+        // Load pixel data into array when file is selected
+        if (file) {
             var fr = new FileReader();
             var img = new Image();
             fr.onload = () => {
@@ -20,19 +19,7 @@ function FileSelector(props) {
                     // Get pixel data from the canvas and send it up
                     var imgd = context.getImageData(0, 0, img.width, img.height);
                     var pix = [...imgd.data];
-                    // var pix = imgd.data;
 
-                    // console.log(typeof pix[0])
-                    // for (var i = 0, n = pix.length; i < n; i += 4) {
-                    //     pix[i] = copy[i]; // red
-                    //     pix[i + 1] = copy[i + 1]; // green
-                    //     pix[i + 2] = copy[i + 2]; // blue
-                    //     pix[i + 3] = 255;
-                    // }
-
-                    // canvas.height = img.height;
-                    // canvas.width = img.width;
-                    // context.putImageData(imgd, 0, 0)
                     props.sendImageData({
                         pix: pix,
                         width: img.width,
