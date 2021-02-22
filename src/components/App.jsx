2e2140e7f9e6aca1bb39c39617fde6d3a5a3e852
Mason Wray <masonwray.mw@gmail.com>
2021-02-21 21:12:30 -0600

    Moved Layer Drawing
    Moved layer drawing function to separate file.

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -6,40 +6,40 @@ import OutputSettings from './OutputSettings';
 import pkg from '../../package.json';
 
 function App() {
-  return (
-    <div>
-      <nav className="navbar navbar-expand-lg navbar-light bg-light">
-        <div className="container-fluid">
-          <div className="navbar-brand">Dot Plotter</div>
-          <div className="d-flex">
-            <div className="nav-item">
-              <div className="nav-link btn-group">
-                <button className="btn btn-outline-secondary btn-sm" disabled={true}>{pkg.version}</button>
-                <a href="https://github.com/MasonWray/Dot-Plotter" className="btn btn-outline-secondary btn-sm"> View on GitHub</a>
-              </div>
+    return (
+        <div>
+            <nav className="navbar navbar-expand-lg navbar-light bg-light">
+                <div className="container-fluid">
+                    <div className="navbar-brand">Dot Plotter</div>
+                    <div className="d-flex">
+                        <div className="nav-item">
+                            <div className="nav-link btn-group">
+                                <button className="btn btn-outline-secondary btn-sm" disabled={true}>{pkg.version}</button>
+                                <a href="https://github.com/MasonWray/Dot-Plotter" className="btn btn-outline-secondary btn-sm"> View on GitHub</a>
+                            </div>
+                        </div>
+                    </div>
+                </div >
+            </nav >
+            <div className="container-fluid">
+                <div className="row">
+                    {/* Left Sidebar */}
+                    <div className="col-sm-3">
+                        <FileSelector />
+                        <Layers />
+                    </div>
+                    {/* Preview Area */}
+                    <div className="col-sm-6">
+                        <Preview />
+                    </div>
+                    {/* Right Sidebar */}
+                    <div className="col-sm-3">
+                        <OutputSettings />
+                    </div>
+                </div>
             </div>
-          </div>
-        </div >
-      </nav >
-      <div className="container-fluid">
-        <div className="row">
-          {/* Left Sidebar */}
-          <div className="col-sm-3">
-            <FileSelector />
-            <Layers />
-          </div>
-          {/* Preview Area */}
-          <div className="col-sm-6">
-            <Preview />
-          </div>
-          {/* Right Sidebar */}
-          <div className="col-sm-3">
-            <OutputSettings />
-          </div>
         </div>
-      </div>
-    </div>
-  );
+    );
 }
 
 export default App;
