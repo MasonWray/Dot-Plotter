b9f8347226f671e51c31029b102f8d605bf500b1
Mason Wray <masonwray.mw@gmail.com>
2021-02-21 21:15:35 -0600

    Moved useElementSize
    Moved useElementSize custom hook to its own file.

--- src/components/Preview.jsx
+++ src/components/Preview.jsx
@@ -2,22 +2,7 @@ import React, { useState, useLayoutEffect, useRef } from 'react';
 import { useSelector } from 'react-redux';
 
 import drawRasterLayers from '../util/drawRasterLayers';
-
-// Get current size of given element when it is resized
-function useElementSize(eRef) {
-    const [size, setSize] = useState([0, 0]);
-    useLayoutEffect(() => {
-        function updateSize() {
-            if (eRef) {
-                setSize([eRef.current.getBoundingClientRect().width, eRef.current.getBoundingClientRect().height]);
-            }
-        }
-        window.addEventListener('resize', updateSize);
-        updateSize();
-        return () => window.removeEventListener('resize', updateSize);
-    }, [eRef]);
-    return size;
-}
+import useElementSize from '../util/useElementSize';
 
 function Preview() {
     const previewRef = useRef();
