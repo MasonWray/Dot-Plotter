804317edc6f3e5982d540c5e1486d557acbcadcc
Mason Wray <masonwray.mw@gmail.com>
2024-01-13 17:21:13 -0600

    Preview UI

--- dplot-web/src/components/Preview.tsx
+++ dplot-web/src/components/Preview.tsx
@@ -4,13 +4,20 @@ import { useAppSelector } from '@/redux/hooks';
 import { useRef } from 'react';
 
 export function Preview() {
-    const previewRef = useRef<HTMLCanvasElement>(null);
+    const previewRef = useRef<HTMLDivElement>(null);
     const canvasRef = useRef<HTMLCanvasElement>(null);
 
     const layers = useAppSelector(state => state.layers.data);
     const settings = useAppSelector(state => state.output);
 
     return (
-        <div></div>
+        <div className="card" ref={previewRef}>
+            <div className="card-header">
+                {"Preview"}
+            </div>
+            <div className="card-body d-flex justify-content-center">
+                <canvas ref={canvasRef} ></canvas>
+            </div>
+        </div >
     )
 }
