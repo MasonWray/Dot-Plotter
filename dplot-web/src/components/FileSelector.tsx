07b63248b6a4236da2c79a48f9059bc1c50f4411
Mason Wray <masonwray.mw@gmail.com>
2024-01-13 13:50:11 -0600

    Add FileSelector UI

--- dplot-web/src/components/FileSelector.tsx
+++ dplot-web/src/components/FileSelector.tsx
@@ -1,5 +1,24 @@
-export function FileSelector(){
-    return(
-        <div></div>
+'use client'
+
+import { ChangeEvent } from 'react';
+
+export function FileSelector() {
+
+    function handleFileChange(e: ChangeEvent<HTMLInputElement>) {
+
+    }
+
+    return (
+        <div className="card">
+            <div className="card-header">File Selector</div>
+            <div className="card-body">
+                <div className="form-group">
+                    <input
+                        type="file"
+                        className="form-control-file"
+                        onChange={handleFileChange} />
+                </div>
+            </div>
+        </div>
     );
 }
