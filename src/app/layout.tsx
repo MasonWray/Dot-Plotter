1a3ce8825da27c07aebbcb0112e74cc006c01b64
Mason Wray <masonwray.mw@gmail.com>
2024-01-20 17:16:11 -0600

    Add Chakra

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,19 +1,28 @@
 'use client'
 
 import { store } from '@/redux/store'
+import { CacheProvider } from '@chakra-ui/next-js'
+import { ChakraProvider } from '@chakra-ui/react'
+import 'bootstrap/dist/css/bootstrap.css'
 import { Inter } from 'next/font/google'
 import { Provider as ReduxProvider } from 'react-redux'
-// import './globals.css'
-import 'bootstrap/dist/css/bootstrap.css'
 
 const inter = Inter({ subsets: ['latin'] })
 
 export default function RootLayout({ children, }: { children: React.ReactNode }) {
   return (
-    <ReduxProvider store={store}>
-      <html lang="en">
-        <body className={inter.className}>{children}</body>
-      </html>
-    </ReduxProvider>
+
+    <html lang="en">
+      <body className={inter.className}>
+        <CacheProvider>
+          <ChakraProvider>
+            <ReduxProvider store={store}>
+              {children}
+            </ReduxProvider>
+          </ChakraProvider>
+        </CacheProvider>
+      </body>
+    </html>
+
   )
 }
